cd8fc9ba01bfff5881b490384d4fabcbf1e45274
Giomar L <giomarlimocaldas1234@gmail.com>
2022-10-23 02:00:42 -0500

    agregando librerias para utilizar BDsqlserver y configurado

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() {
+                new Autor() { id = 1, nombre = "Giomar"},
+                new Autor() { id = 2, nombre = "Gerson"}
+            };
+        }
+    }
+}
--- WebApiAutores/Startup.cs
+++ WebApiAutores/Startup.cs
@@ -1,4 +1,7 @@
-﻿namespace WebApiAutores
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace WebApiAutores
 {
     public class Startup
     {
@@ -12,9 +15,14 @@
         public void ConfigureServices(IServiceCollection services) 
         {
             services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIAutores", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
